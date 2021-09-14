80cddce958e87fc955614d01bac83534e4917417
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-14 18:35:52 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,10 +5,13 @@
 # Command: wget https://raw.githubusercontent.com/tarekzoka/iptosat/main/installer.sh -O - | /bin/sh
 wget -O /tmp/iptosat_1.8_all.ipk "https://github.com/tarekzoka/-xstreamity/raw/main/eenigma2-plugin-extensions-iptosat_1.8_all.ipk"
 wait
-opkg install /tmp/*.ipk
+#!/bin/sh
+opkg -i /tmp/*.ipk
 wait
-opkg install --force-overwrite /tmp/*.ipk
+dpkg -i --force-overwrite /tmp/*.ipk
 wait
 sleep 2;
+;
+
 
 
