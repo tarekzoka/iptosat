71ca36203cefb8402db74d8387367d192aa1213b
tarekhanfy <65197356+tarekzoka@users.noreply.github.com>
2021-09-18 02:10:57 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
-# ==============================================
-# SCRIPT : DOWNLOAD AND INSTALL iptosat #
-# =====================================================================================================================
-# Command: wget https://raw.githubusercontent.com/tarekzoka/iptosat/main/installer.sh -O - | /bin/sh
-wget -O /tmp/iptosat_1.8_all.ipk "https://github.com/tarekzoka/-xstreamity/raw/main/eenigma2-plugin-extensions-iptosat_1.8_all.ipk"
+#
+# SCRIPT : DOWNLOAD AND INSTALL XCPLUGIN
+#
+###########################################
+#command: wget https://raw.githubusercontent.com/tarekzoka/iptosat/main/installer.sh -O - | /bin/sh
+ #
+MY_EM="*********************************************************************************"
+wget -O /var/volatile/tmp/XCplugin.tar.gz "https://raw.githubusercontent.com/tarekzoka/iptosat/main/XCplugin.tar.gz"
 wait
-#!/bin/sh
-opkg -i-O /tmp/*.ipk
+echo $MY_EM
+######      INSTALL PLUGIN Xcplugin ###
+tar xzvpf /tmp/*.tar.gz -C /
 wait
-dpkg -i --force-overwrite /tmp/*.ipk
-wait
-sleep 2;
-;
-
+echo $MY_EM
+#######   REMOVE PACKAGE FROM TMP ###
+rm -r /var/volatile/tmp/XCplugin.tar.gz 
+echo "========================================================================" 	
+echo ". WELCOME TO PLUGIN XCPLUGIN "
+echo $MY_EM
+	echo " PLUGIN INSTALLED SUCCESSFULLY "
+echo " UPLOADED BY EMIL_NABIL " 
+echo "======================================================"
+echo "       YOUR DEVICE WELL RESTART Now " 
+echo "****************************************"
+wait 3;
+init 4
+init 3
+exit 0
 
 
